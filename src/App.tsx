1c7ebb1cb5e28496f30c3ba8aa7742d025382ee3
coldrain-f <intmain__@naver.com>
2024-02-18 00:07:55 +0900

    Antd 동작 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Form, Typography, Input, Space } from "antd";
+const {Title } = Typography;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Title>Dashboard</Title>
+          <Form name='wrap' labelCol={{flex: '110px'}} labelAlign='left' labelWrap wrapperCol={{flex: 1}} colon={false} 
+            style={{maxWidth: 600}}>
+            <Form.Item label="ID" name={'username'} rules={[{ required: true }]}>
+              <Input/>
+            </Form.Item>
+            
+            <Form.Item label='Password' name={'password'} rules={[{required: true}]}>
+              <Input />
+            </Form.Item>
+            <Form.Item>
+              
+                <Button type='primary' htmlType='submit' block>Login</Button>    
+            </Form.Item>
+          </Form>
       </header>
     </div>
   );
